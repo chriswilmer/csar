05f29617ce8fcfdea28ed157adc718fe53eca4ad
chriswilmer <c.wilmer@gmail.com>
2013-08-13 17:49:27 -0500

    added a new file

---
+++ csar_v0_1_0.py
@@ -0,0 +1,36 @@
+# Crystal-Search-And-Replace (CSAR)
+#
+# Python re-implementation of "FunctionalizeThis!" 
+# Written originally by Chris Wilmer in 2010, reimplemented in Aug. 2013
+# Free and open-source, license definition pending
+
+import sys
+import time
+
+from math import sqrt
+import math
+e
+print("Hello Python Three!")
+
+print(sys.version_info)
+
+# immediate todo list
+#   load parameters
+#       - collision detection in replace pattern (on off tolerance)
+#   load JSON structure
+#   load JSON "search" pattern
+#   load JSON "replace" pattern
+#   find (and mark) "search" patterns in the structure
+#   replace "search" patterns with "replace" patterns
+#   output JSON structure
+#   output debugging information (# of patterns found, # replaced)
+#   write documentation
+# 
+# "stretch" todo list
+#   overlapping patterns
+#   make it work on 0-D, 1-D, and 2-D structures
+#   space groups...
+#   selection scripting (i.e., find pattern, expand selection, delete core, etc.)
+#   add structure relaxation with MMTK
+#   add charge averaging scheme
+
